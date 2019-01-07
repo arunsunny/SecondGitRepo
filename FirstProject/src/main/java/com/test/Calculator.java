015ad023a0f8ba27b04447abcae61cd29e56d12a
induco <induco@induco5>
2019-01-07 19:21:11 +0530

    second commit

---
+++ FirstProject/src/main/java/com/test/Calculator.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Calculator {
+	public static void main(String[] args) {
+		System.out.println("hi hello world ghjhgjj");
+	}
+
+}
--- SecondProject/src/main/java/com/arunsoft/TestApp.java
+++
@@ -1,7 +0,0 @@
-package com.arunsoft;
-
-public class TestApp {
-	public static void main(String[] args) {
-		System.out.println("hi hello world");
-	}
-}
